1f913dfe817b61a50cd51347d6a41f3624763d88
Mikevill20 <meonyema@gmail.com>
2026-07-30 07:47:06 +0200

    add power-trader keyboard navigation & quick actions

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { useShortcuts } from "../hooks/useShortcuts";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -19,15 +20,13 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
   children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+}: Readonly<{ children: React.ReactNode; }>) {
+  const { HelpModal } = useShortcuts({});
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         {children}
+        {HelpModal}
       </body>
     </html>
   );
